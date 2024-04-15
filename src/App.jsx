6bd8411ba8afcbedc4e258420fb0feb620eb5aa7
nameerfarooq <nameerfarooquit@gmail.com>
2024-04-15 16:55:41 +0500

    Simple Motions

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+const App = () => {
+  const [isVisible, setIsVisible] = useState(true);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div
+        style={{
+          display: "grid",
+          placeContent: "center",
+          height: "100vh",
+          gap: "0.8rem",
+        }}
+      >
+        <button onClick={() => setIsVisible(!isVisible)}>Show/Hide</button>
+        <AnimatePresence mode="popLayout">
+          {isVisible && (
+            <motion.div
+              initial={{
+                rotate: "0deg",
+                scale: 0,
+                y: 0,
+                x: 0,
+              }}
+              animate={{
+                rotate: "180deg",
+                scale: 1,
+                y: [0, 150, -150, -150, 0, 0, 0, 0, 0],
+                x: [0, 0, 0, 0, 0, 150, -150, -150, 0],
+              }}
+              exit={{
+                rotate: "0deg",
+                scale: 0,
+                y: 0,
+                x: 0,
+              }}
+              transition={{
+                duration: 1,
+                ease: "backInOut",
+                times: [0, 0.25, 0.35, 0.4, 0.5, 0.6, 0.85, 0.9, 1],
+              }}
+              style={{
+                width: "150px",
+                height: "150px",
+                backgroundColor: "black",
+              }}
+            >
+              Hello
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
