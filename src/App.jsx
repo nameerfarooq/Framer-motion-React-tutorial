b29ad5f30768ea7716ccbbfe75bed1a8a5bea8d9
nameerfarooq <nameerfarooquit@gmail.com>
2024-04-15 18:13:03 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,52 @@ const App = () => {
           )}
         </AnimatePresence>
       </div>
+      <div
+        style={{
+          display: "grid",
+          placeContent: "center",
+          height: "100vh",
+          gap: "0.8rem",
+        }}
+      >
+        <button onClick={() => setIsVisible(!isVisible)}>Show/Hide</button>
+        <AnimatePresence mode="popLayout">
+          {isVisible && (
+            <motion.div
+              initial={{
+                rotate: "0deg",
+                scale: 0,
+                y: 0,
+                x: 0,
+              }}
+              animate={{
+                rotate: "180deg",
+                scale: 1,
+                y: [0, 150, -150, -150, 0, 0, 0, 0, 0],
+                x: [0, 0, 0, 0, 0, 150, -150, -150, 0],
+              }}
+              exit={{
+                rotate: "0deg",
+                scale: 0,
+                y: 0,
+                x: 0,
+              }}
+              transition={{
+                duration: 1,
+                ease: "backInOut",
+                times: [0, 0.25, 0.35, 0.4, 0.5, 0.6, 0.85, 0.9, 1],
+              }}
+              style={{
+                width: "150px",
+                height: "150px",
+                backgroundColor: "black",
+              }}
+            >
+              Hello
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
     </div>
   );
 };
